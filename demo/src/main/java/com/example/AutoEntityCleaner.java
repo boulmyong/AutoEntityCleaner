a101657eeafa4db4ac33fd2a3152705241e6db88
boulmyong <wontop10@gmail.com>
2025-08-15 02:22:46 +0900

    한국어 지원및, 최적화전 경고 추가

--- demo/src/main/java/com/example/AutoEntityCleaner.java
+++ demo/src/main/java/com/example/AutoEntityCleaner.java
@@ -5,26 +5,41 @@ import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class AutoEntityCleaner extends JavaPlugin {
 
-    private int cleanInterval;
-    private List<EntityType> entitiesToClean;
+    private int cleanIntervalSeconds;
+    private List<Class<? extends Entity>> entitiesToClean;
+    private boolean warningsEnabled;
+    private int countdown;
+
+    // Message fields
+    private String msgManualCleanStart;
+    private String msgManualCleanFinish;
+    private String msgNoPermission;
+    private String msgWarning;
+
+    private static final List<Integer> WARNING_TIMES = Arrays.asList(10, 5, 4, 3, 2, 1);
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         loadConfigValues();
 
-        if (cleanInterval > 0) {
-            Bukkit.getScheduler().runTaskTimer(this, this::cleanEntities, cleanInterval, cleanInterval);
-            getLogger().info("Auto-cleaning of entities scheduled every " + (cleanInterval / 20) + " seconds.");
+        if (cleanIntervalSeconds > 0) {
+            // Run the countdown timer every second (20 ticks)
+            Bukkit.getScheduler().runTaskTimer(this, this::countdownTick, 0L, 20L);
+            getLogger().info("Auto-cleaning of entities scheduled every " + cleanIntervalSeconds + " seconds.");
         } else {
             getLogger().info("Auto-cleaning of entities is disabled.");
         }
@@ -42,10 +57,12 @@ public class AutoEntityCleaner extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("cleanentities")) {
             if (sender.hasPermission("autocleaner.clean")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgManualCleanStart));
                 int cleanedCount = cleanEntities();
-                sender.sendMessage(ChatColor.GREEN + "Successfully cleaned " + cleanedCount + " entities.");
+                String finalMessage = msgManualCleanFinish.replace("{count}", String.valueOf(cleanedCount));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', finalMessage));
             } else {
-                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgNoPermission));
             }
             return true;
         }
@@ -53,34 +70,79 @@ public class AutoEntityCleaner extends JavaPlugin {
     }
 
     private void loadConfigValues() {
-        this.cleanInterval = getConfig().getInt("clean-interval", 6000);
-        List<String> entityNames = getConfig().getStringList("entities-to-clean");
+        FileConfiguration config = getConfig();
+        // Interval is in ticks, convert to seconds for our logic
+        this.cleanIntervalSeconds = config.getInt("clean-interval", 6000) / 20;
+        this.countdown = this.cleanIntervalSeconds;
+
+        List<String> entityNames = config.getStringList("entities-to-clean");
         this.entitiesToClean = entityNames.stream()
                 .map(name -> {
                     try {
-                        return EntityType.valueOf(name.toUpperCase());
+                        EntityType type = EntityType.valueOf(name.toUpperCase());
+                        return type.getEntityClass();
                     } catch (IllegalArgumentException e) {
                         getLogger().warning("Invalid entity type in config.yml: " + name);
                         return null;
                     }
                 })
-                .filter(java.util.Objects::nonNull)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+
+        this.warningsEnabled = config.getBoolean("warnings.enabled", true);
+
+        // Load messages based on language setting
+        String lang = config.getInt("language", 1) == 1 ? "ko" : "en";
+        this.msgManualCleanStart = config.getString("messages.manual_clean_start." + lang);
+        this.msgManualCleanFinish = config.getString("messages.manual_clean_finish." + lang);
+        this.msgNoPermission = config.getString("messages.no_permission." + lang);
+        this.msgWarning = config.getString("messages.warning." + lang);
+    }
+
+    private void countdownTick() {
+        if (warningsEnabled && WARNING_TIMES.contains(countdown)) {
+            String message = msgWarning.replace("{time}", String.valueOf(countdown));
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
+
+        if (countdown <= 0) {
+            runCleaningTask();
+            this.countdown = this.cleanIntervalSeconds; // Reset countdown
+        } else {
+            countdown--;
+        }
+    }
+
+    private void runCleaningTask() {
+        getLogger().info("Starting automatic entity cleaning task...");
+        long startTime = System.currentTimeMillis();
+        int cleanedCount = cleanEntities();
+        long endTime = System.currentTimeMillis();
+        getLogger().info("Finished automatic entity cleaning task. Cleaned " + cleanedCount + " entities in " + (endTime - startTime) + "ms.");
     }
 
     private int cleanEntities() {
-        int cleanedCount = 0;
+        int totalCleanedCount = 0;
+        if (entitiesToClean.isEmpty()) {
+            return 0;
+        }
+
         for (World world : Bukkit.getWorlds()) {
-            for (Entity entity : world.getEntities()) {
-                if (entitiesToClean.contains(entity.getType())) {
-                    entity.remove();
-                    cleanedCount++;
-                }
+            int worldCleanedCount = 0;
+            @SuppressWarnings("unchecked")
+            Class<? extends Entity>[] entityClasses = entitiesToClean.toArray(new Class[0]);
+            Collection<Entity> entities = world.getEntitiesByClasses(entityClasses);
+
+            for (Entity entity : entities) {
+                entity.remove();
+                worldCleanedCount++;
             }
+
+            if (worldCleanedCount > 0) {
+                getLogger().info("Cleaned " + worldCleanedCount + " entities from world: " + world.getName());
+            }
+            totalCleanedCount += worldCleanedCount;
         }
-        if (cleanedCount > 0) {
-            getLogger().info("Cleaned " + cleanedCount + " entities.");
-        }
-        return cleanedCount;
+        return totalCleanedCount;
     }
 }
